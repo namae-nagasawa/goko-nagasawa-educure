670e7b5164694c36b3c9a18b7677e56b8909e774
mituk <mituk@LAPTOP-E97OCCG2>
2024-08-30 03:32:32 +0900

    提出になります

---
+++ src/main/java/jp/co/example/SpringDiTaskJdbcApplication.java
@@ -0,0 +1,27 @@
+package jp.co.example;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import jp.co.example.entity.User;
+import jp.co.example.service.UserService;
+
+@SpringBootApplication
+
+public class SpringDiTaskJdbcApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(SpringDiTaskJdbcApplication.class, args);
+
+		UserService userService = context.getBean(UserService.class);
+		List<User> list = userService.findAll();
+		for (User u : list) {
+			System.out.println(u.getUserInfo());
+
+		}
+	}
+
+}
