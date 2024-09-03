1cec3e10ae69150cfe43f80ccdcb314e94191ec7
mituk <mituk@LAPTOP-E97OCCG2>
2024-09-03 10:18:30 +0900

    途中経過になります。

---
+++ src/main/java/jp/co/example/SpringWebMvcSelectApplication.java
@@ -0,0 +1,13 @@
+package jp.co.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcSelectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvcSelectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/jp/co/example/controller/form/IndexForm.java
@@ -0,0 +1,15 @@
+package jp.co.example.controller.form;
+
+public class IndexForm {
+
+	private Integer userId;
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(Integer userId) {
+		this.userId=userId;
+	}
+	
+}
